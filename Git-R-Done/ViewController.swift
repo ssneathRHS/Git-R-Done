70357813277ba8a72f7e4aba4cf80b7375ea9646
Stephanie Sneath <ssneath@ridgenet.org>
2019-06-28 09:31:47 -0500

    try 5

--- Git-R-Done/ViewController.swift
+++ Git-R-Done/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
+    //addded a view will Appear
     override func viewWillAppear(_ animated: Bool) {
         
     }
